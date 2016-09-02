c312fd309837816c8099248ffe5f493bdf4d02e6
Nick <nicholas.clement@colorado.edu>
2016-09-02 10:40:21 -0600

    fixed file

--- HW 1/ds-hw-master/wrangling/districts.py
+++ HW 1/ds-hw-master/wrangling/districts.py
@@ -24,7 +24,6 @@ def all_states(lines):
     in one line of Python.
     """
 
-<<<<<<< HEAD
     # print(lines["STATE"])
 
     x = [0]*len(lines)
@@ -37,30 +36,6 @@ def all_states(lines):
     x = set(x)
     print (x)
     return set(["Alabama"])
-=======
-    with open('2014_election_results.csv', 'rb') as csvfile:
-
-
-    # Complete this function
-    # print(lines)
-    # x = { lines[3] : lines[3] for each in lines }
-    # for each in lines:
-    #     x = {
-    #     'STATES':
-    #     }
-
-
-    # for i in range(len(lines)):
-    #     # if lines[i-1]["STATE"] != (lines[i]["STATE"]) and (lines[i]["STATE"]) != "":
-    #     x.append(str(lines[i]["STATE"]))
-    #
-    #     i+=1
-
-    print(set(x["Alabama"]))
-
-
-    return set(x["Alabama"])
->>>>>>> ef7b9c03a6c11b734ed8009383061984ef8076a5
 
 def all_state_rows(lines, state):
     """
