97d438504ced162e50db4298b54339fc103e6569
Nick <Nicholas.clement@colorado.edu>
2016-10-09 12:01:06 -0600

    going to office hours

--- Assignment_3/stat_test/bigrams.py
+++ Assignment_3/stat_test/bigrams.py
@@ -68,22 +68,22 @@ def chisquare_pvalue(obs, ex):
     # b = (((obs[0][1] - ex[0][1])**2)/obs[0][1])
     # c = (((obs[1][0] - ex[1][0])**2)/obs[1][0])
     # d = (((obs[1][1] - ex[1][1])**2)/obs[1][1])
-
-
+    print((chisquare(array(obs), f_exp = array(ex), ddof=1, axis=0))[1])
+    return((chisquare(array(obs), f_exp = array(ex), ddof=1, axis=0))[1])
 # notice the chi square import
     # swag = np.array(obs, ex).T
-    print(obs, ex)
-    newObs = newEx = []
-    for x, y in obs, ex:
-        newObs.append(x)
-        newEx.append(y)
-
-    print(array(obs))
-    print(array(ex))
-
-    print(chisquare(obs, f_exp=ex, ddof=1, axis=None)[1])
-    return((chisquare(obs, f_exp=ex, ddof=1, axis=None)[1]))
-    # return (chisquare(array(obs, f_exp = ex)))
+    # print(obs, ex)
+    # newObs = newEx = []
+    # for x, y in obs, ex:
+    #     newObs.append(x)
+    #     newEx.append(y)
+    #
+    # print(array(obs))
+    # print(array(ex))
+    #
+    # print(chisquare(obs, f_exp=ex, ddof=1, axis=None)[1])
+    # return((chisquare(obs, f_exp=ex, ddof=1, axis=None)[1]))
+    # # return (chisquare(array(obs, f_exp = ex)))
 
 
 
